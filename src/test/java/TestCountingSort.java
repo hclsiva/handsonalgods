c54ecea17691a81c90b53e179551283181e6ff80
Sivashankar Ravisankar <Sivashankar.Ravisankar@volvocars.com>
2021-07-18 20:58:41 +0200

    updated tests for 1000 entries

--- src/test/java/TestCountingSort.java
+++ src/test/java/TestCountingSort.java
@@ -3,7 +3,10 @@ package sorting.countingsort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+import java.util.Arrays;
 
 public class TestCountingSort {
     CountingSort sorting = null;
@@ -17,10 +20,20 @@ public class TestCountingSort {
         return inputArray;
     }
     private int[] getOneThousandEntries(){
-        return null;
+        int[] unsortedArray = generateRandomNumber(1,9,1000);
+        return unsortedArray;
     }
     private int[] getOneMillionEntries(){
         return null;
+    }
+    private int[] generateRandomNumber(int min, int max,int size){
+        Random random = new Random();
+        int[] unsortedArray = new int[size];
+        for(int i = 0; i < size;i++) {
+            int temp = random.nextInt(max - min +1 ) + min; 
+            unsortedArray[i]= temp;
+        }
+        return unsortedArray;
     }
     @Test
     @DisplayName("Counting Sort with Simple input")
@@ -29,5 +42,14 @@ public class TestCountingSort {
         int[] expectedOutput = new int[]{1,1,1,2,2,3,3,3,4,5,5,6,6,6,7,7,8,8,9};
         int[] output = sorting.sort(inputArray);
         assertArrayEquals(output,expectedOutput);
+    }
+    @Test
+    @DisplayName("Counting Sort with One Thousand Entries")
+    public void testCountingSortWithOneThousandInput(){
+        int[] unsortedArray = getOneThousandEntries();
+        int[] countingSortOutput = sorting.sort(unsortedArray);
+        Arrays.sort(unsortedArray);
+        int[] expectedOutput = unsortedArray;
+        assertArrayEquals(countingSortOutput,expectedOutput);
     }
 }
